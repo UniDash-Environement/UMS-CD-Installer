9d9894434a3e7e8bb527dd7813187baad6dfb028
Pikatsuto <pikatsuto@gmail.com>
2022-09-05 15:05:37 +0200

    remove lvm-extend and retract command

--- install/service/system/command.sh
+++ install/service/system/command.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/bash
 
 function installCommand() {
-    cp ./asset/bin/lvm-extend.sh /bin/lvm-extend
-    chmod +x /bin/lvm-extend
-    cp ./asset/bin/lvm-retract.sh /bin/lvm-retract
-    chmod +x /bin/lvm-retract
-
     cp ./asset/bin/add-ssd-key.sh /bin/add-ssd-key
     chmod +x /bin/add-ssd-key
 
