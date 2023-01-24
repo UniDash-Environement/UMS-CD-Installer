5cb42962d611ada38734f285d5883b9b12efc3e0
Jonathan <contact@grafikart.fr>
2023-01-24 11:59:42 +0100

    Update apt repository

--- install/config/apt.sh
+++ install/config/apt.sh
@@ -3,10 +3,12 @@
 # changer les dépot proxmox pour ceut de debian 11
 
 function aptSourceList() {
+    # Remove all sources.list.d
     rm -rf /etc/apt/sources.list.d/*
     mkdir /etc/apt/sources.list.d
     echo "" > /etc/apt/sources.list
 
+    # Add Debian 11 Bullseye
     echo "deb http://deb.debian.org/debian bullseye main contrib non-free
     deb-src http://deb.debian.org/debian bullseye main contrib non-free
 
@@ -16,8 +18,9 @@ function aptSourceList() {
     deb http://deb.debian.org/debian bullseye-updates main contrib non-free
     deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free" > /etc/apt/sources.list
 
-    apt-get update -y
-    apt-get upgrade -y
+    # Upgrade And install utils package
+    apt-get update -y && apt-get upgrade -y
+    apt install -y sudo fish
 }
 
 $1
