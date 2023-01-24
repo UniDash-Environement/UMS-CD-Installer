f519bf537aa2f624616bd92d4519123622e8886c
Jonathan <contact@grafikart.fr>
2023-01-24 11:53:15 +0100

    Update apt repository

--- install/config/apt.sh
+++ install/config/apt.sh
@@ -7,29 +7,14 @@ function aptSourceList() {
     mkdir /etc/apt/sources.list.d
     echo "" > /etc/apt/sources.list
 
-    echo "#Debian Officiel:
-deb http://deb.debian.org/debian/                       bullseye main
-deb http://deb.debian.org/debian/                       bullseye-updates main contrib
+    echo "deb http://deb.debian.org/debian bullseye main contrib non-free
+    deb-src http://deb.debian.org/debian bullseye main contrib non-free
 
-#Debian Source:
-deb-src http://deb.debian.org/debian/                   bullseye main
-deb-src http://deb.debian.org/debian/                   bullseye-updates main contrib
+    deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+    deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
 
-#Backports:
-deb http://deb.debian.org/debian        bullseye-backports main contrib non-free
-
-#Backports Source:
-deb-src http://deb.debian.org/debian    bullseye-backports main contrib non-free
-
-#Security
-deb http://security.debian.org/debian-security          bullseye-security main contrib
-deb-src http://security.debian.org/debian-security      bullseye-security main contrib"\
-     > /etc/apt/sources.list.d/debian.list
-
-    echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-subscription" \
-     > /etc/apt/sources.list.d/pve-install-repo.list
-    wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg \
-     -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
+    deb http://deb.debian.org/debian bullseye-updates main contrib non-free
+    deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free" > /etc/apt/sources.list
 
     apt-get update -y
     apt-get upgrade -y
